ff258878447b51519a9f8b17c449ede12c3900b9
adilafsar99 <adilafsar99@gmail.com>
2026-03-20 12:46:38 +0500

    Set up an event listener that toggles the read status of a book.

--- script.js
+++ script.js
@@ -27,6 +27,17 @@ const removeFromLibrary = function (id) {
     library = library.filter(book => book.id !== id);
 }
 
+const changeStatus = function (e) {
+    const bookCard = e.target.closest('.book');
+    const bookId = bookCard.dataset.bookId;
+    for (const book of library) {
+        if (book.id == bookId) {
+            book.changeStatus();
+            showBooks();
+        }
+    }
+}
+
 const getBookData = function (event) {
     event.preventDefault();
     addBookModal.close();
@@ -47,71 +58,70 @@ const showBooks = function () {
     }
     else {
         instruction.classList.remove('show');
+        booksList.innerHTML = '';
         library.forEach((book) => {
-            if (!visibleBooks.includes(book.id)) {
-                let titleValue = book.title;
-                let authorValue = book.author;
-                let pagesValue = book.pages;
-                let isReadValue = book.isRead;
-                let id = book.id;
+            let titleValue = book.title;
+            let authorValue = book.author;
+            let pagesValue = book.pages;
+            let isReadValue = book.isRead;
+            let id = book.id;
 
-                const title = document.createElement('p');
-                title.classList.add('title');
-                title.textContent = titleValue;
-                const author = document.createElement('p');
-                author.classList.add('author');
-                author.textContent = authorValue;
-                const pages = document.createElement('p');
-                pages.classList.add('pages');
-                pages.textContent = pagesValue;
-                const status = document.createElement('p');
-                status.classList.add('status');
-                status.textContent = isReadValue ? 'Read' : 'Not read';
+            const title = document.createElement('p');
+            title.classList.add('title');
+            title.textContent = titleValue;
+            const author = document.createElement('p');
+            author.classList.add('author');
+            author.textContent = authorValue;
+            const pages = document.createElement('p');
+            pages.classList.add('pages');
+            pages.textContent = pagesValue;
+            const status = document.createElement('p');
+            status.classList.add('status');
+            status.textContent = isReadValue ? 'Read' : 'Not read';
 
-                const infoSection = document.createElement('div');
-                infoSection.classList.add('info-section');
-                infoSection.appendChild(title);
-                infoSection.appendChild(author);
-                infoSection.appendChild(pages);
-                infoSection.appendChild(status);
-                book = document.createElement('div');
-                book.classList.add('book');
-                book.setAttribute('data-book-id', id);
-                book.appendChild(infoSection);
+            const infoSection = document.createElement('div');
+            infoSection.classList.add('info-section');
+            infoSection.appendChild(title);
+            infoSection.appendChild(author);
+            infoSection.appendChild(pages);
+            infoSection.appendChild(status);
+            book = document.createElement('div');
+            book.classList.add('book');
+            book.setAttribute('data-book-id', id);
+            book.appendChild(infoSection);
 
-                const toggleIcon = document.createElement('span');
-                toggleIcon.classList.add('fa-regular', 'fa-circle-check');
-                const toggleButton = document.createElement('button');
-                toggleButton.classList.add('toggle-button');
-                toggleButton.appendChild(toggleIcon);
-                const editIcon = document.createElement('span');
-                editIcon.classList.add('fa-regular', 'fa-pen-to-square');
-                const editButton = document.createElement('button');
-                editButton.classList.add('edit-button');
-                editButton.appendChild(editIcon);
-                const deleteIcon = document.createElement('span');
-                deleteIcon.classList.add('fa-regular', 'fa-trash-can');
-                const deleteButton = document.createElement('button');
-                deleteButton.classList.add('delete-button');
-                deleteButton.appendChild(deleteIcon);
+            const toggleIcon = document.createElement('span');
+            toggleIcon.classList.add('fa-regular', 'fa-circle-check');
+            const toggleButton = document.createElement('button');
+            toggleButton.classList.add('toggle-button');
+            toggleButton.addEventListener('click', changeStatus);
+            toggleButton.appendChild(toggleIcon);
+            const editIcon = document.createElement('span');
+            editIcon.classList.add('fa-regular', 'fa-pen-to-square');
+            const editButton = document.createElement('button');
+            editButton.classList.add('edit-button');
+            editButton.appendChild(editIcon);
+            const deleteIcon = document.createElement('span');
+            deleteIcon.classList.add('fa-regular', 'fa-trash-can');
+            const deleteButton = document.createElement('button');
+            deleteButton.classList.add('delete-button');
+            deleteButton.appendChild(deleteIcon);
 
-                const buttonSection = document.createElement('div');
-                buttonSection.classList.add('button-section');
-                buttonSection.appendChild(toggleButton);
-                buttonSection.appendChild(editButton);
-                buttonSection.appendChild(deleteButton);
-                book.appendChild(buttonSection);
+            const buttonSection = document.createElement('div');
+            buttonSection.classList.add('button-section');
+            buttonSection.appendChild(toggleButton);
+            buttonSection.appendChild(editButton);
+            buttonSection.appendChild(deleteButton);
+            book.appendChild(buttonSection);
 
-                booksList.appendChild(book);
-                visibleBooks.push(id);
-            }
-        })
+            booksList.appendChild(book);
+        }
+        )
     }
 }
 
 
 const library = [];
-const visibleBooks = [];
 
 //addToLibrary('No Longer Human', 'Osamu Dazai', '177', false);
 
