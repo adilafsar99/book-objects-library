62108abb4c652a01396d3026f858acc4d85d7484
adilafsar99 <adilafsar99@gmail.com>
2026-03-20 09:17:57 +0500

    Align the open modal to be in the center of the page,

--- script.js
+++ script.js
@@ -16,31 +16,21 @@ Books.prototype.updateProperty = function (property, value) {
     this[property] = value;
 }
 
-const library = [];
-
-let book = new Books('No Longer Human', 'Osamu Dazai', '177', false);
-
-// console.log(book);
-// console.log(book.isRead);
-// book.changeStatus();
-// console.log(book.isRead);
-
-// book.updateProperty('title', 'Brave New World');
-// console.log(book);
-
-// library.push(book);
-// console.log(library);
-
 const addToLibrary = function (title, author, pages, isRead) {
      let newBook = new Books(title, author, pages, isRead);
      newBook.id = crypto.randomUUID();
      library.push(newBook);
 }
 
-// addToLibrary(book.title, book.author, book.pages, book.status);
-// console.log(library);
-
-
 const removeFromLibrary = function (id) {
    library = library.filter(book => book.id !== id);
 }
+
+
+const library = [];
+
+let book = new Books('No Longer Human', 'Osamu Dazai', '177', false);
+
+const addBookButton = document.querySelector('#add-button');
+const addBookModal = document.querySelector('#add-book-modal');
+addBookButton.addEventListener('click', () => addBookModal.showModal());
