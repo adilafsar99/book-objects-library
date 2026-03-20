eea199f733aeda023b8f5df6504ab1b2d1818d0a
adilafsar99 <adilafsar99@gmail.com>
2026-03-20 10:06:28 +0500

    Set up an event listener to collect book data on modal form submission.

--- script.js
+++ script.js
@@ -20,10 +20,25 @@ const addToLibrary = function (title, author, pages, isRead) {
      let newBook = new Books(title, author, pages, isRead);
      newBook.id = crypto.randomUUID();
      library.push(newBook);
+     return newBook;
 }
 
 const removeFromLibrary = function (id) {
    library = library.filter(book => book.id !== id);
+}
+
+const getBookData = function (event) {
+   event.preventDefault();
+   addBookModal.close();
+   const inputNodes = document.querySelectorAll('.form-input');
+   const inputs = Array.from(inputNodes);
+   let title = inputs[0].value;
+   let author = inputs[1].value;
+   let pages = inputs[2].value;
+   let isRead = inputs[3].checked ? true : false;
+   let newBook = addToLibrary(title, author, pages, isRead);
+   addBookForm.reset();
+   console.log(newBook);
 }
 
 
@@ -33,4 +48,8 @@ let book = new Books('No Longer Human', 'Osamu Dazai', '177', false);
 
 const addBookButton = document.querySelector('#add-button');
 const addBookModal = document.querySelector('#add-book-modal');
+const addBookForm = document.querySelector('#add-book-form');
+const closeModalButton = document.querySelector('#modal-button');
+
 addBookButton.addEventListener('click', () => addBookModal.showModal());
+addBookForm.addEventListener('submit', getBookData)
